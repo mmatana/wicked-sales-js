8193d8f7260d7dfef90683c72d759e9ebc67a545
mmatana <milojevic.maja@gmail.com>
2020-09-15 19:41:29 -0700

    saved query to a const inside of app.get.

--- server/index.js
+++ server/index.js
@@ -20,8 +20,9 @@ app.get('/api/health-check', (req, res, next) => {
     .catch(err => next(err));
 });
 
+const viewAllProducts = 'select "productId", "name", "price", "image", "shortDescription" from "products"';
 app.get('/api/products', (req, res, next) => {
-  db.query('select "productId", "name", "price", "image", "shortDescription" from "products"')
+  db.query(viewAllProducts)
     .then(result => res.json(result.rows))
     .catch(err => next(err));
 });
